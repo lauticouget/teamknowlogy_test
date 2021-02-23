065924ce0cc403ccd0accf4667c2b837e9929da0
lauticouget <lauticouget@gmail.com>
2021-02-22 23:08:42 -0300

    master | port deleted

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,15 +1,14 @@
 const mysql = require('mysql');
 const util = require('util');
 require('dotenv').config();
-console.log(  process.env.HOST,
-   process.env.USER,
-   process.env.DATABASE,
-   process.env.DB_PORT);
+console.log(process.env.HOST,
+  process.env.USER,
+  process.env.DATABASE
+);
 var mysqlConnection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
-  database: process.env.DATABASE,
-  port: process.env.DB_PORT
+  database: process.env.DATABASE
 });
 
 mysqlConnection.connect();
